0859f340da5be75952f8ae3bc95705449db06b7c
n00bDr01d <oscarabuawad@hotmail.com>
2024-03-26 15:05:11 -0400

    Primer Programa MRU

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Calculadora de MRUA");
+            Console.Write("Ingrese la velocidad inicial (m/s): ");
+            double velocidadInicial = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la aceleración (m/s^2): ");
+            double aceleracion = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el tiempo (s): ");
+            double tiempo = Convert.ToDouble(Console.ReadLine());
+
+            double distancia = velocidadInicial * tiempo + 0.5 * aceleracion * Math.Pow(tiempo, 2);
+            Console.WriteLine($"La distancia recorrida es: {distancia} metros");
